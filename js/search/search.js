f052e744b862f8205779f4d617d15b7d121862d2
chrisalexander <chris.alexander@import.io>
2014-05-27 18:29:51 +0100

    Search functionality, results (no style)

--- js/search/search.js
+++ js/search/search.js
@@ -3,16 +3,24 @@
  */
 var search = angular.module("ca-search", []);
 
+search.config(['$locationProvider', function($locationProvider) {
+    $locationProvider.html5Mode(true);
+}]);
+
 /**
  * Controller for the search box
  */
-search.controller("searchForm", function($scope) {
+search.controller("searchForm", function($scope, $location, $location, searchService) {
 
-	$scope.query = "";
+	$scope.query = $location.search().hasOwnProperty("query") ? $location.search()["query"] : "";
 
 	$scope.executeSearch = function(event, query) {
-		event.preventDefault();
-		
+		event ? event.preventDefault() : false;
+		searchService.query(query);
+	}
+
+	if ($scope.query.length) {
+		$scope.executeSearch(false, $scope.query);
 	}
 
 });
@@ -20,21 +28,62 @@ search.controller("searchForm", function($scope) {
 /**
  * Controller for the search results
  */
+search.controller("searchResults", function($scope, $timeout, searchService) {
 
-/**
- * Directive for the page and initialisation
- */
+	$scope.search = searchService;
+
+	$scope.searchDone = false;
+
+	$scope.currentSearchTimeout = false;
+
+	$scope.$on("loading", function(_, loading) {
+		if (loading) {
+			$scope.searchDone = false;
+			if ($scope.currentSearchTimeout) {
+				clearTimeout($scope.currentSearchTimeout);
+			}
+		} else {
+			$scope.searchDone = true;
+			if ($scope.currentSearchTimeout) {
+				clearTimeout($scope.currentSearchTimeout);
+			}
+			$scope.currentSearchTimeout = $timeout(function() {
+				$scope.searchDone = false;
+			}, 1000);
+		}
+	});
+
+	$scope.getSnippet = function(result) {
+		if (!result.hasOwnProperty("content")) {
+			return "";
+		}
+		var content = [].concat(result["content"]).join(" ");
+		var targetLength = 100;
+		if (content.length < targetLength) {
+			return content;
+		} else {
+			return content.substr(0, targetLength) + "...";
+		}
+	}
+
+});
 
 /**
  * Service for doing searches and managing the data
  */
-search.factory("searchService", function() {
+search.factory("searchService", function($http, $rootScope) {
 
 	// Which page we are currently looking at
 	var page = 1;
 
+	// How many items per page to run
+	var itemsPerPage = 10;
+
 	// What the input query is
 	var query = "";
+
+	// The computed max number of pages for this query
+	var maxPage = 1;
 
 	// What the current page of data is
 	var data = [];
@@ -42,17 +91,51 @@ search.factory("searchService", function() {
 	// Whether or not we are loading
 	var loading = true;
 
-	var executeSearch = function() {
-		loading = true;
+	var setLoadingState = function(state) {
+		loading = state;
+		$rootScope.$broadcast("loading", state);
 	}
 
-	return {
+	var executeSearch = function(clearExistingResults) {
+		setLoadingState(true);
+
+		if (clearExistingResults) {
+			data = [];
+		}
+
+		$http({
+			"method": "GET",
+			"url": "https://chrisalexanderdotcodotuk.east-us.azr.facetflow.io/blog/post/_search?q=" + encodeURIComponent(query) + "&df=title&df=content&from=" + ((page-1) * itemsPerPage) + "&size=" + itemsPerPage
+		}).success(function(resultData, status, headers, config) {
+			var tmpData = [];
+			resultData.hits.hits.map(function(hit) {
+				tmpData.push(hit._source);
+			});
+			data = tmpData;
+			updateMaxPages(resultData.hits.total);
+			loading = false;
+			setLoadingState(false);
+		}).error(function(resultData, status, headers, config) {
+			data = [];
+			setLoadingState(false);
+		});
+	}
+
+	var updateMaxPages = function(totalResults) {
+		maxPage = Math.ceil(totalResults / itemsPerPage);
+	}
+
+	var iface = {
 		"go": {
 			"next": function() {
-
+				if (!iface.has.next()) {
+					return;
+				}
+				page++;
+				executeSearch();
 			},
 			"previous": function() {
-				if (page <= 1) {
+				if (!iface.has.previous()) {
 					return;
 				}
 				page--;
@@ -61,14 +144,18 @@ search.factory("searchService", function() {
 		},
 		"has": {
 			"next": function() {
-
+				return query && data.length && maxPage >= page + 1;
 			},
 			"previous": function() {
-
+				return page > 1;
 			}
 		},
-		"query": function(query) {
-
+		"query": function(q) {
+			query = q;
+			executeSearch(true);
+		},
+		"page": function() {
+			return page;
 		},
 		"data": function() {
 			return data;
@@ -77,5 +164,7 @@ search.factory("searchService", function() {
 			return loading;
 		}
 	}
+
+	return iface;
 
 });
--- search/index.md
+++ search/index.md
@@ -2,7 +2,10 @@
 layout: base
 heading: 'Chris Alexander - On Engineering - Search'
 subheading: 'Search the On Engineering blog of Chris Alexander - talking about tech, engineering, robots, and everything involving electrons'
+stylesheet: 'search'
 ---
+
+{% raw %}
 
 <div ng-app="ca-search">
 
@@ -13,7 +16,28 @@ subheading: 'Search the On Engineering blog of Chris Alexander - talking about t
 		<input type="submit" value="Search">
 	</form>
 
+	<div class="loading-container" ng-controller="searchResults">
+		<div class="loading" ng-show="search.isLoading()">Loading...</div>
+		<div class="done" ng-show="searchDone">Done!</div>
+	</div>
+
+	<div class="results" ng-controller="searchResults">
+		<div class="result" ng-repeat="result in search.data()">
+			<h3>{{result.title}}</h3>
+			<p>{{getSnippet(result)}}</p>
+			<p><a ng-href="{{result._url">{{result._url}}</a></p>
+		</div>
+
+		<div class="page">
+			<a href="#" class="previous" ng-show="search.has.previous()" ng-click="search.go.previous()">&laquo;</a>
+			<span class="page_number ">Page {{ search.page() }}</span>
+			<a href="#" class="next" ng-show="search.has.next()" ng-click="search.go.next()">&raquo;</a>
+		</div>
+	</div>
+
 </div>
+
+{% endraw %}
 
 <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular.min.js"></script>
 <script src="/js/search/search.js"></script>
